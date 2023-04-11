38da0d4b8a6fed267fa196270307c0b8990c1673
Sabuj <sabujrouth2000@gmail.com>
2023-04-11 13:09:19 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "color-primary": "#112541",
         "color-primary-light": "#020726",
         "color-primary-dark": "#010417",
-        "color-secondary": "#ff7d3b"
+        "color-secondary": "#ff7d3b",
         "color-gray": "#333",
         "color-white": "#fff",
         "color-blob": "#A427DF",
